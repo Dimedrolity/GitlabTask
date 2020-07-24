9440cd47a30793910d3afa1e7bf2885271e7a232
DRed <dima.redkin.1999@gmail.com>
2020-07-24 16:36:38 +0500

    Добавил пару тестов конфига

--- GitlabTask.Tests/ConfigTests.cs
+++ GitlabTask.Tests/ConfigTests.cs
@@ -12,13 +12,29 @@ namespace GitlabTask.Tests
             var projects = config.GetProjects();
             Assert.IsTrue(projects != null && projects.Any());
         }
-        
+
         [Test]
         public void ConfigPatternsIsNotEmpty()
         {
             var config = new Config();
             var patterns = config.GetPatternsOfExcludedTitle();
             Assert.IsTrue(patterns != null && patterns.Any());
+        }
+
+        [Test]
+        public void ConfigGitlabDomainNameExists()
+        {
+            var config = new Config();
+            var domainName = config.GetGitlabDomainName();
+            Assert.IsTrue(domainName != null);
+        }
+
+        [Test]
+        public void ConfigPersonalAccessTokenExists()
+        {
+            var config = new Config();
+            var personalAccessToken = config.GetPersonalAccessToken();
+            Assert.IsTrue(personalAccessToken != null);
         }
     }
 }
